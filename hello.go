6c8101dac7b44e3f649e727507bd8196f3c0154b
Lílian Rangel <105670314+lilian-rangel@users.noreply.github.com>
2023-04-13 15:56:52 -0300

    :construction: feat: declaring variables

--- hello.go
+++ hello.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var nome string = "Lilian"
+	var dias int = 2
+	var versao float32 = 2.3
+	fmt.Println("Olá, sra", nome,"Este é seu", dias, "dia aprendendo go")
+	fmt.Println("Este é a versão", versao, "do seu código")
+
 }
